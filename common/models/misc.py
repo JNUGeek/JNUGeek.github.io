131e0e28e6ea3e69b9f78e79a34035738b7f2fcd
Patrickctyyx <873948000@qq.com>
2016-12-13 23:21:51 +0800

    12.13 Weekly Build:     Modify login.html with template~     Beautify userinfo.html

--- common/models/misc.py
+++ common/models/misc.py
@@ -11,7 +11,7 @@ import uuid
 class Session(db.Model):
     __tablename__ = current_app.config["TABLE_PREFIX"] + 'session'
 
-    sid = db.Column(db.String(128), primary_key=True,
+    sid = db.Column(db.String(150), primary_key=True,
                     default=lambda: str(uuid.uuid4()))
     data = db.Column(db.LargeBinary)  # 二进制
     expiry = db.Column(db.DateTime)
